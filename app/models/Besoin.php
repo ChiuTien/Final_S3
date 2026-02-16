3ccdbcd2132bf6409d0dad9a8874fbcba1c51693
ChiuTien <chiumandresychristian@gmail.com>
2026-02-16 15:03:38 +0300

    Classe Besoin.php

--- app/models/Besoin.php
+++ app/models/Besoin.php
@@ -1,3 +1,32 @@
 <?php 
-    
+    namespace app\models;  
+
+    class Besoin {
+    //Attributs
+        private $idBesoin;
+        private $idTypeBesoin;
+        private $valBesoin;
+    //Constructeur
+        public function __construct() {}
+    //Setters
+        public function setIdBesoin($id) {
+            $this->idBesoin = $id;
+        }
+        public function setIdTypeBesoin($id) {
+            $this->idTypeBesoin = $id;
+        }
+        public function setValBesoin($val) {
+            $this->valBesoin = $val;
+        }
+    //Getters
+        public function getIdBesoin() {
+            return $this->idBesoin;
+        }
+        public function getIdTypeBesoin() {
+            return $this->idTypeBesoin;
+        }
+        public function getValBesoin() {
+            return $this->valBesoin;
+        }
+    }
 ?>
