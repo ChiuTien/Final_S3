06927659899d9c8dfd99a268bbfab6e867fcbffe
VlexandreMG <batmanhoodie23mahery@gmail.com>
2026-02-16 15:54:12 +0300

    Changement de classe Type et ajout repository

---
+++ app/repository/RepType.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\repository;
+
+use Flight;
+use PDO;
+
+
+
+class RepType
+{
+    private PDO $db;
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function addType($type) {
+        $sql = "INSERT INTO type (valType) VALUES (:valType)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':valType', $type->getValType(), PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    public function removeType($type) {
+        $sql = "DELETE FROM type WHERE idType = :idType";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':idType', $type->getIdType(), PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function getTypeById($id) {
+        $sql = "SELECT * FROM type WHERE idType = :idType";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':idType', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllTypes() {
+        $sql = "SELECT * FROM type";
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+?>
