82f9eaffafe40db8ccad2219cf4d3a68bdf5944d
ChiuTien <chiumandresychristian@gmail.com>
2026-02-16 22:14:52 +0300

    Modification d'un des scripts

--- sql/16_02_2026_05_donne.sql
+++ sql/16_02_2026_05_donne.sql
@@ -5,12 +5,12 @@ INSERT INTO Ville (idRegion, valVille) VALUES (1,'Antananarivo'),(2,'Toamasina')
 INSERT INTO Type (valType) VALUES ('Nature'),('Matériaux'),('Argent');
 INSERT INTO Produit (valProduit, idType) VALUES ('Riz',1),('Huile',1),('Tôles',2),('Clous',2);
 
-INSERT INTO Besoin (valBesoin, idType, idVille, quantite, prixUnitaire, urgence, dateBesoin) VALUES
-('Riz',1,1,500,2000,'important','2026-02-16 08:00:00'),
-('Huile',1,1,200,5000,'important','2026-02-16 08:05:00'),
-('Tôles',2,3,100,25000,'urgent','2026-02-16 09:00:00'),
-('Riz',1,2,300,2000,'normal','2026-02-15 12:00:00');
-
+INSERT INTO Besoin (valBesoin, idType) VALUES
+('Riz',1),
+('Huile',1),
+('Tôles',2),
+('Riz',1);
+    
 INSERT INTO Don (dateDon, totalPrix) VALUES
 ('2026-02-16 10:00:00',200000),
 ('2026-02-16 09:30:00',50000),
