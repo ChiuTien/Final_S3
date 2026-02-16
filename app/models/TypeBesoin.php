8977e6e56867c014d80ef8da2bd65fe3f22541a1
VlexandreMG <batmanhoodie23mahery@gmail.com>
2026-02-16 14:59:45 +0300

    Classe TypeBesoin

---
+++ app/models/TypeBesoin.php
@@ -0,0 +1,29 @@
+<?php 
+namespace app\models;
+
+class TypeBesoin {
+    private $idTypeBesoin;
+    public $valTypeBesoin;
+    public $description;
+
+    public function __construct() {
+        
+    }
+
+    public function setIdTypeBesoin($idTypeBesoin) {
+        $this->idTypeBesoin = $idTypeBesoin;
+    }
+
+    public function getIdTypeBesoin() {
+        return $this->idTypeBesoin;
+    }
+
+    public function setValTypeBesoin($valTypeBesoin) {
+        $this->valTypeBesoin = $valTypeBesoin;
+    }
+
+    public function getValTypeBesoin() {
+        return $this->valTypeBesoin;
+    }
+}
+?>
