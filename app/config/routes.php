b51f3b9e1b9052d1bb66eed92f791e25437dd1de
tsikychristelle <tsikychristelle4@gmail.com>
2026-02-16 17:08:39 +0300

    dons

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,7 +4,10 @@ use app\controllers\ApiExampleController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
-
+use app\models\Don;
+use app\models\Donnation;
+use app\controllers\ControllerDon;
+use app\controllers\ControllerDonnation;
 /** 
  * @var Router $router 
  * @var Engine $app
@@ -13,18 +16,20 @@ use flight\net\Router;
 // This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function(Router $router) use ($app) {
 
-	$router->get('/', function() use ($app) {
-		$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
-	});
+    // Route pour la page d'accueil
+    $router->get('/', function() use ($app) {
+        $app->render('welcome');
+    });
 
-	$router->get('/hello-world/@name', function($name) {
-		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
-	});
+    // Route pour l'affichage des dons
+    $router->get('/donsAffichage', function() use ($app) {
+        $controllerDon = new ControllerDon();
+        $dons = $controllerDon->getAllDons();
+        
+        $controllerDonnation = new ControllerDonnation();
+        $donnations = $controllerDonnation->getAllDonnation();
 
-	$router->group('/api', function() use ($router) {
-		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
-		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
-		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
-	});
-	
+        $app->render('donsAffichage', ['dons' => $dons, 'donnations' => $donnations]);
+    });
+
 }, [ SecurityHeadersMiddleware::class ]);
