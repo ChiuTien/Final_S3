5783add894676595fdc215782f731684b17bd3c6
ChiuTien <chiumandresychristian@gmail.com>
2026-02-16 14:22:43 +0300

    Table Besoin et EquivalenceBesoin

--- sql/16_02_2026_01_base.sql
+++ sql/16_02_2026_01_base.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE BNGRC;
 USER BNGRC;
+CREATE OR REPLACE TABLE Besoin(
+    idBesoin INT PRIMARY KEY AUTO_INCREMENT,
+    idTypeBesoin INT,
+    valBesoin VARCHAR(100)
+);
+
+CREATE OR REPLACE TABLE EquivalenceBesoin(
+    idEquivalenceBesoin INT PRIMARY KEY AUTO_INCREMENT,
+    idBesoin INT,
+    prixUnitaire DOUBLE,
+    quantite DOUBLE,
+    val VARCHAR(50)
+);
